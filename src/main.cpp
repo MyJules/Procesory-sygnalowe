71e6292b40e1ac095b675ef0b9c8de22c923ed11
MyJules <mudryk200@gmail.com>
2022-10-11 00:01:14 +0200

    Read values

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,50 @@
 ﻿#include <string>
 #include <iostream>
+#include <vector>
 
 #include <cxxopts.hpp>
 
+struct AmplifyOptions 
+{
+	int one;
+	int two;
+
+	friend std::istream& operator>>(std::istream& is, AmplifyOptions& person)
+	{
+		is >> person.one >> person.two;
+		return is;
+	}
+};
+
 int main(int argc, char *argv[])
 {
-	cxxopts::Options options("AudioProcessor", "Program for audio processing");
+	cxxopts::Options mainOptions("AudioProcessor", "Program for audio processing");
 
-	options.add_options()
-		("f,file", "Audio file name", cxxopts::value<std::string>())
-		("o,output", "Output file", cxxopts::value<std::string>()->default_value("processed"))
-		("a,amplify", "Amplify audio", cxxopts::value<bool>()->default_value("false"))
+	std::string inputFile;
+	std::string outputFile;
+	int amplify = 0;
+
+	mainOptions
+		.allow_unrecognised_options()
+		.add_options()
+		("i,input", "Input audio file name", cxxopts::value<std::string>(inputFile))
+		("o,output", "Output file", cxxopts::value<std::string>(outputFile)->default_value("processed.wav"))
+		("a,amplify", "Amplify audio (dB)", cxxopts::value<int>(amplify))
 		//TODO: add effects what we want to create
 		;
-	options.allow_unrecognised_options();
 
 	if (argc == 1)
 	{
-		std::cout << options.help() << std::endl;
+		std::cout << mainOptions.help() << std::endl;
 		return 0;
 	}
 
-	cxxopts::ParseResult result = options.parse(argc, argv);
 
-	for (auto &res : result.arguments()) 
-	{
-		std::cout <<  res.key() << " : " << res.value() << std::endl;
-	}
+	cxxopts::ParseResult result = mainOptions.parse(argc, argv);
+
+	std::cout << "Input file: " << inputFile << std::endl;
+	std::cout << "Output file: " << outputFile << std::endl;
+	std::cout << "Amplify: " << amplify << std::endl;
 
 	return 0;
 }
