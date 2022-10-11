e5b3ebf1a40a3f291970b98ba106fb6b5e3981a8
MyJules <mudryk200@gmail.com>
2022-10-11 21:18:38 +0200

    Example struct parsing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 
 #include <cxxopts.hpp>
 
+struct CustomOpts {
+	int a;
+	int b;
+
+	friend std::istream& operator>>(std::istream& is, CustomOpts& opts)
+	{
+		is >> opts.a >> opts.b;
+		return is;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, CustomOpts& opts)
+	{
+		os << opts.a << " : " << opts.b;
+		return os;
+	}
+};
+
 int main(int argc, char *argv[])
 {
 	cxxopts::Options mainOptions("AudioProcessor", "Program for audio processing");
@@ -11,6 +28,7 @@ int main(int argc, char *argv[])
 	std::string inputFile;
 	std::string outputFile;
 	int amplify = 0;
+	CustomOpts opts;
 
 	mainOptions
 		.allow_unrecognised_options()
@@ -18,6 +36,7 @@ int main(int argc, char *argv[])
 		("i,input", "Input audio file name", cxxopts::value<std::string>(inputFile))
 		("o,output", "Output file", cxxopts::value<std::string>(outputFile)->default_value("processed.wav"))
 		("a,amplify", "Amplify audio (dB)", cxxopts::value<int>(amplify))
+		("type", "Type params, example use --type \"a b\" ", cxxopts::value<CustomOpts>(opts))
 		//TODO: add effects what we want to create
 		;
 
@@ -28,7 +47,7 @@ int main(int argc, char *argv[])
 	catch(const cxxopts::exceptions::parsing& exception) 
 	{
 		std::cerr << exception.what() << std::endl;
-		return -1;
+		return EXIT_FAILURE;
 	}
 
 	if (argc == 1)
@@ -40,12 +59,13 @@ int main(int argc, char *argv[])
 	if (inputFile.empty())
 	{
 		std::cerr << "No input file provided, please enter file path" << std::endl;
-		return 1;
+		return EXIT_FAILURE;
 	}
 
 	std::cout << "Input file: " << inputFile << std::endl;
 	std::cout << "Output file: " << outputFile << std::endl;
 	std::cout << "Amplify: " << amplify << std::endl;
+	std::cout << "Custom opts: " << opts << std::endl;
 
 	return 0;
 }
