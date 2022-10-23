783689c21781b53bf8d21fdb4700471483b12641
MyJules <mudryk200@gmail.com>
2022-10-23 21:20:39 +0200

    Fixed submodules

--- src/console/ConsoleProcessor.cpp
+++ src/console/ConsoleProcessor.cpp
@@ -1,37 +1,41 @@
 #include "ConsoleProcessor.h"
 
-#include <filesystem>
-
-#include <kfr/base.hpp>
-#include <kfr/dft.hpp>
-#include <kfr/dsp.hpp>
 #include <kfr/io.hpp>
+
+#include <filesystem>
 
 namespace console
 {
 	int processConsoleParam(const console::ConsoleParser::ConsoleParam& params)
 	{
-		std::filesystem::path filePath(params.inputFile);
-		if (!std::filesystem::exists(filePath)) 
-		{
-			std::cerr << "Input file dosn't exists!\n";
-			return EXIT_FAILURE;
-		}
-
-		kfr::audio_reader_wav<float> reader(kfr::open_file_for_reading(params.inputFile));
-
 		if (params.helpFlag)
 		{
 			std::cout << params.helpInfo << std::endl;
 			return EXIT_SUCCESS;
 		}
 
-		if (params.inputFile.empty())
+		std::filesystem::path filePath(params.inputFile);
+		if (!std::filesystem::exists(filePath)) 
 		{
-			std::cerr << "No input file provided, please enter file path" << std::endl;
+			std::cerr << "Please enter valid input file\n";
 			return EXIT_FAILURE;
 		}
 
+		kfr::audio_reader_wav<kfr::fbase> wavReader(kfr::open_file_for_reading(filePath));
+		kfr::univector<kfr::fbase> wav = wavReader.read(wavReader.format().length);
+
+		if (params.trackInfo)
+		{
+			std::cout << "File name: " << filePath.filename() << std::endl;
+			std::cout << "SempleRate: " << wavReader.format().samplerate << std::endl;
+			std::cout << "Channels: " << wavReader.format().channels << std::endl;
+			std::cout << "Length: " << wavReader.format().length << std::endl;
+			std::cout << "Duration (s): " << wavReader.format().length / wavReader.format().samplerate << std::endl;
+			std::cout << "Bit depth: " << kfr::audio_sample_bit_depth(wavReader.format().type) << std::endl;
+
+			return EXIT_SUCCESS;
+		}
+		
 		// Handle effect 
 		if (params.echoParams)
 		{
@@ -54,14 +58,10 @@ namespace console
 			delay->process();
 		}
 
-		if (params.trackInfo) 
-		{
-			std::cout << "File name: " << filePath.filename() << std::endl;
-			std::cout << "SempleRate: " << reader.format().samplerate << std::endl;
-			std::cout << "Channels: " << reader.format().channels << std::endl;
-			std::cout << "Length: " << reader.format().length << std::endl;
-			std::cout << "Duration (s): " << reader.format().length / reader.format().samplerate << std::endl;
-			std::cout << "Bit depth: " << kfr::audio_sample_bit_depth(reader.format().type) << std::endl;
-		}
+		// At the end we want to save file
+		kfr::audio_writer_wav<kfr::fbase> wavWriter(kfr::open_file_for_writing(params.outputFile), wavReader.format());
+
+		wavWriter.write(wav);
+		wavWriter.close();
 	}
 }
